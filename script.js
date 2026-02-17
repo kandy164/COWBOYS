e0635ad24f50567c876fbd07eaf983f401382253
kandy164 <emmanuelkandy66@gmail.com>
2026-02-17 16:44:05 +0200

    Update and rename script.js.txt to script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function surprise() {
   const tips = [
     "Music boosts your mood instantly!",
     "Listening to music while studying can improve focus.",
-    "Try exploring new genres — you might find a hidden favorite!",
+    "Try exploring new genres â€” you might find a hidden favorite!",
     "Playing an instrument sharpens your brain."
   ];
   const randomTip = tips[Math.floor(Math.random() * tips.length)];
